c387f5b9f905c8b351a83b2ce1c8b9bbffac34a5
Lucas Nunes Pinto Pinheiro <lucasnpinheiro@gmail.com>
2019-10-02 09:56:26 -0300

    Correção de rede unknown ssid

--- src/android/wifiwizard2/WifiWizard2.java
+++ src/android/wifiwizard2/WifiWizard2.java
@@ -1173,6 +1173,18 @@ public class WifiWizard2 extends CordovaPlugin {
         serviceInfo = info.getBSSID();
       } else {
         serviceInfo = info.getSSID();
+
+        if(serviceInfo != null && serviceInfo.contains("unknown ssid")) {
+            NetworkInfo wifiInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            String wifiName = wifiInfo.getExtraInfo();
+            if (wifiName.startsWith("\"")) {
+                wifiName = wifiName.substring(1, wifiName.length());
+            }
+            if (wifiName.endsWith("\"")) {
+                wifiName = wifiName.substring(0, wifiName.length() - 1);
+            }
+            serviceInfo = wifiName;
+        }
       }
   
       if (serviceInfo == null || serviceInfo.isEmpty() || serviceInfo == "0x") {
